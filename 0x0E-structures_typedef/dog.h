a13b939b4cd15322e18ad84cbd31c9cb53dfac94
Hajar <hajar1445@gmail.com>
2023-04-17 17:15:32 -0700

    adding task0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - Structure for dog information
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Description: structure that stores information about a dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
