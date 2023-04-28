0c9715620ad5a49e02998714a7ea8b3fa09b3918
Hajar <hajar1445@gmail.com>
2023-04-28 22:04:49 +0100

    adding task

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -3,8 +3,17 @@
 
 /**
  * free_list - frees a linked list
+ * @head: list_t list to be freed
  */
 void free_list(list_t *head)
 {
+	list_t *temp;
 
+	while (head)
+	{
+		temp = head->next;
+		free(head->str);
+		free(head);
+		head = temp;
+	}
 }	
